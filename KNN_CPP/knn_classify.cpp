fd7077e01195d76e342ba5df45a2a25eac465c68
Anthony Harris <33495441+KillerBOB999@users.noreply.github.com>
2020-01-21 15:57:15 -0600

    Finished initialization, added relevant information, and defined data types

--- KNN_CPP/knn_classify.cpp
+++ KNN_CPP/knn_classify.cpp
@@ -1,36 +1,24 @@
 // INFORMATION--------------------------------------------------------------------------
 // DEVELOPER:        Anthony Harris
-// GITHUB:           Anthony999
+// GITHUB:           https://github.com/KillerBOB999/KNN_CPP
 // DATE:             21 January 2019
 // PURPOSE:          KNN algorithm implementation in C++ for CSC736: Machine Learning.
 //--------------------------------------------------------------------------------------
 
-// /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
 
 #include <iostream>
+using std::cin;
+using std::cout;
+
 #include <vector>
 using std::vector;
 
-int main(int argc, char* argv[])
+using std::tuple;
+
+int main(int argc, char* argv[]) // invoked by: knn_classify pendigits_training pendigits_test <k>
 {
-    const int classIndex = 16;
-    vector<vector<int>> trainingData;
-    vector<vector<int>> testData;
-    std::cout << argv[1] << std::endl;
+    vector<tuple<vector<int>, int>> trainingData;
+    vector<tuple<vector<int>, int>> testData;
+    const int k = (int)argv[3];
 }
-
-
-// FUNCTION DESCRIPTION-----------------------------------------------------------------
-        // Function Name:    distanceMeasure
-        // Parameters:  entry1
-        //                  Use:    Represents an entry in the trainingDataSet for use in
-        //                          calculating the distance between it and entry2
-        //              entry2
-        //                  Use:    Represents the current entry of the testDataSet for use
-        //                          in calculating the distance between it and entry1
-        // Returns:          A floating point representation of the 'distance measure' of the
-        //                   2 entries.
-        // Description:      For each element e of the entries, find the summation of the 
-        //                   absolute value of the difference bewteen e1 and e2 and then take
-        //                   the Nth root of that sum
-        //--------------------------------------------------------------------------------------
